22481ff282bc1ed7e600c331b39b80fb062fd7f7
Laloui Abdelkrim <lalouikarim@gmail.com>
2020-08-25 16:01:29 +0100

    Add files via upload

---
+++ Movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM movies JOIN stars ON movies.id = stars.movie_id JOIN ratings ON ratings.movie_id = movies.id WHERE stars.person_id IN (SELECT id from people WHERE name = "Chadwick Boseman") ORDER BY rating DESC LIMIT 5;
---
+++ Movies/README.md
@@ -0,0 +1,2 @@
+# Extracting data from a SQLite database
+# These programs extract different informations about movies from a SQLite database provided by IMDB
